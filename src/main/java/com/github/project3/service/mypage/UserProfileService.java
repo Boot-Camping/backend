ab76a26b9d71a827bc3df481a6419964a9b47e3c
junyoung22 <126755514+junyoung22@users.noreply.github.com>
2024-08-14 20:05:27 +0900

    Feat: 사용자ì유저회 구현

--- src/main/java/com/github/project3/service/mypage/UserProfileService.java
+++ src/main/java/com/github/project3/service/mypage/UserProfileService.java
@@ -1,11 +1,14 @@
 package com.github.project3.service.mypage;
 
 import com.github.project3.dto.mypage.UserProfileResponse;
+import com.github.project3.entity.user.CashEntity;
 import com.github.project3.entity.user.UserEntity;
+import com.github.project3.entity.user.UserImageEntity;
 import com.github.project3.repository.mypage.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,7 +29,9 @@ public class UserProfileService {
                 userEntity.getName(),
                 userEntity.getPassword(),
                 userEntity.getEmail(),
-                userEntity.getTel()
+                userEntity.getTel(),
+                userEntity.getImages().stream().map(UserImageEntity::getImageUrl).collect(Collectors.toList()),
+                userEntity.getCash().stream().max(Comparator.comparing(CashEntity::getTransactionDate)).map(CashEntity::getBalanceAfterTransaction).orElse(0)
         );
     }
 }
