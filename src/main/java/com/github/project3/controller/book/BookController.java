38ebf38893e86da9e44f859911d49099484a262d
OneCloudd <whdlfdns12@gmail.com>
2024-08-22 17:30:36 +0900

    Refactor : bookcontroller swagger annotation ì

--- src/main/java/com/github/project3/controller/book/BookController.java
+++ src/main/java/com/github/project3/controller/book/BookController.java
@@ -3,6 +3,7 @@ package com.github.project3.controller.book;
 import com.github.project3.dto.book.BookInquiryResponse;
 import com.github.project3.dto.book.BookRegisterRequest;
 import com.github.project3.service.book.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,7 @@ public class BookController {
      * @param bookRegisterRequest   예약 등록 요청 정보를 담고 있는 객체
      * @return 예약이 성공적으로 완료되었을 때 "예약이 완료되었습니다." 메시지를 포함한 ResponseEntity 반환
      */
+    @Operation(summary = "예약 등록", description = "캠핑장 예약을 등록합니다.")
     @PostMapping("/{campId}/{userId}")
     public ResponseEntity<String> registerBook(@PathVariable Integer campId, @PathVariable Integer userId,
                                         @RequestBody BookRegisterRequest bookRegisterRequest){
@@ -42,6 +44,7 @@ public class BookController {
      * @param userId  예약 취소를 요청하는 사용자의 ID
      * @return 예약이 성공적으로 취소되었을 때 "예약이 취소되었습니다."와 환불 금액을 포함한 ResponseEntity 반환
      */
+    @Operation(summary = "예약 취소", description = "기존 캠핑장 예약을 취소합니다.")
     @PutMapping("/{bookId}/{userId}")
     public ResponseEntity<String> cancelBook(@PathVariable Integer bookId, @PathVariable Integer userId){
         Integer cash = bookService.cancelBook(bookId, userId);
@@ -54,6 +57,7 @@ public class BookController {
      * @param userId  예약 내역을 조회하려는 사용자의 ID
      * @return 예약 내역 리스트를 포함한 ResponseEntity 반환
      */
+    @Operation(summary = "예약 조회", description = "사용자의 캠핑장 예약 내역을 조회합니다.")
     @GetMapping("/{userId}")
     public ResponseEntity<List<BookInquiryResponse>> inquiryBook(@PathVariable Integer userId){
         List<BookInquiryResponse> response = bookService.inquiryBook(userId);
