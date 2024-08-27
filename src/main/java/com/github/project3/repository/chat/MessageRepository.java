f1867df9c3548c7b297b30ecfb5ca1b1f63296df
OneCloudd <whdlfdns12@gmail.com>
2024-08-27 19:15:42 +0900

    Feat : MessageRepository

---
+++ src/main/java/com/github/project3/repository/chat/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.github.project3.repository.chat;
+
+import com.github.project3.entity.chat.MessageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+}
