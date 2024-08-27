6a76d2362107f8521dcf54d8834edc9f558f72a7
OneCloudd <whdlfdns12@gmail.com>
2024-08-27 16:59:17 +0900

    Feat : Entity

---
+++ src/main/java/com/github/project3/entity/chat/ChatRoomEntity.java
@@ -0,0 +1,29 @@
+package com.github.project3.entity.chat;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "ChatRoom")
+public class ChatRoomEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
--- src/main/java/com/github/project3/handler/ChatWebSocketHandler.java
+++ src/main/java/com/github/project3/handler/ChatWebSocketHandler.java
@@ -1,7 +1,6 @@
 package com.github.project3.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
