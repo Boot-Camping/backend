2e451a145b02df2e630d8ae2f087104a6caa99a7
OneCloudd <whdlfdns12@gmail.com>
2024-08-28 20:55:07 +0900

    Feat : Jpa AutoConfiguration

--- src/main/java/com/github/project3/config/JpaConfig.java
+++ src/main/java/com/github/project3/config/JpaConfig.java
@@ -3,19 +3,11 @@ package com.github.project3.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories(
-        basePackages = {"com.github.project3.repository.user", "com.github.project3.repository.mypage"
-        , "com.github.project3.repository.book", "com.github.project3.repository.bookDate", "com.github.project3.repository.camp"
-        , "com.github.project3.repository.cash", "com.github.project3.repository.review","com.github.project3.repository.admin"
-        , "com.github.project3.repository.reply", "com.github.project3.repository.chat"}
-)
-
 public class JpaConfig {
     @Autowired
     private DataSourceConfig dataSourceConfig;
