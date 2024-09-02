f1b8b8f16e6a14334aa2c372c97eaf96b9d35b1f
junyoung22 <126755514+junyoung22@users.noreply.github.com>
2024-09-02 13:05:49 +0900

    ãRefactor: 스케줄러 Component 수정

--- src/main/java/com/github/project3/service/admin/AdminBalanceScheduler.java
+++ src/main/java/com/github/project3/service/admin/AdminBalanceScheduler.java
@@ -1,14 +1,10 @@
 package com.github.project3.service.admin;
 
-import com.github.project3.entity.user.UserEntity;
-import com.github.project3.entity.user.enums.Role;
-import com.github.project3.repository.user.UserRepository;
-import com.github.project3.service.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class AdminBalanceScheduler {
 
